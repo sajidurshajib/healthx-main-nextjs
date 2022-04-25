6685567b47c90c3751e94dcfee8a43ec9f81855f
.env.prod <redoyrahman02@gmail.com>
2022-04-25 10:32:03 +0000

    a

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,6 +4,6 @@ cd /var/www/healthxweb
 rm -rf /var/www/healthxweb/package-lock.json
 rm -rf /var/www/healthxweb/.next/cache
 
-npm install 
-npm run build 
+/root/.nvm/versions/node/v14.19.1/bin/npm install 
+/root/.nvm/versions/node/v14.19.1/bin/npm run build 
 /root/.nvm/versions/node/v14.19.1/bin/pm2 restart all
