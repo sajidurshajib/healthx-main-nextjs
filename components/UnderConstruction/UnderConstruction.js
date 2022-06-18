e195415343a6d2721134fe52f551abb3deba6670
sajidurshajib <sajidur.inbox@gmail.com>
2022-06-18 10:50:41 +0600

    added: under construction

---
+++ components/UnderConstruction/UnderConstruction.js
@@ -0,0 +1,14 @@
+import classes from './UnderConstruction.module.css'
+
+const UnderConstruction = () => {
+    return (
+        <div className={classes.UnderConstruction}>
+            <div className={classes.Wrapper}>
+                <h2>We are working to give you a better experience.</h2>
+                <h2>Please stay with us till we live.</h2>
+            </div>
+        </div>
+    )
+}
+
+export default UnderConstruction
