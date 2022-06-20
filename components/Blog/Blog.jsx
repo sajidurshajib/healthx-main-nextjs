21018e518b4b0713a878bc8796aa792e0a23830d
morshedul-antor <antor.morshedul@gmail.com>
2022-06-20 17:21:32 +0600

    feat: blog components add

---
+++ components/Blog/Blog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import BlockGrid from './BlockGrid/BlockGrid'
+
+export default function Blog() {
+    return (
+        <div>
+            <BlockGrid />
+        </div>
+    )
+}
