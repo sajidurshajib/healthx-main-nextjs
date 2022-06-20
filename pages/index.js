d0cc0e122ab686949ae36b99cc14ae58a7abe03f
morshedul-antor <antor.morshedul@gmail.com>
2022-06-20 11:37:30 +0600

    modify: slider

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Hero, ServiceBox, SliderPartner, UnderConstruction } from '../components'
+import { Footer, Hero, ServiceBox, SliderPartner, UnderConstruction } from '../components'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -9,10 +9,11 @@ export default function Home() {
             <Head>
                 <title>HEALTHx</title>
             </Head>
-            <UnderConstruction />
-            {/* <Hero />
+            {/* <UnderConstruction /> */}
+            <Hero />
             <ServiceBox />
-            <SliderPartner /> */}
+            <SliderPartner />
+            <Footer />
         </div>
     )
 }
