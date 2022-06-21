b9112dfd44fc8a2565aa429a2329733c43f8128a
morshedul-antor <antor.morshedul@gmail.com>
2022-06-21 12:04:21 +0600

    export: hero

--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 export { default as MainNav } from './Nav/MainNav/MainNav'
-export { default as Hero } from './Hero/Hero'
+export { default as Hero } from './Hero/Hero.jsx'
 export { default as Services } from './Services/Services'
 export { default as ServiceBox } from './ServiceBox/SeriveBox'
 export { default as SliderPartner } from './SliderPartner/SliderPartner'
