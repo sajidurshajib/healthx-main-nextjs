fcc617d3068391516db362b4ff0bbb2167e86c66
Jahid11186 <zahidhasansamim@gmail.com>
2022-06-21 12:02:02 +0600

    comments: removed

--- components/index.js
+++ components/index.js
@@ -6,11 +6,3 @@ export { default as SliderPartner } from './SliderPartner/SliderPartner'
 export { default as Blog } from './Blog/Blog'
 export { default as Footer } from './Footer/Footer'
 export { default as WhyUs } from './WhyUs/WhyUs'
-
-// export { default as Services } from './Services/Services'
-// export { default as ServiceBox } from './ServiceBox/SeriveBox'
-// export { default as SliderPartner } from './SliderPartner/SliderPartner'
-// export { default as Footer } from './Footer/Footer'
-// export { default as UnderConstruction } from './UnderConstruction/UnderConstruction'
-
-// export { Hero, Footer, SeriveBox, Services, SliderPartner, UnderConstruction }
