e072d49477736c394fef9651fca565cef8a383fb
Jahid11186 <zahidhasansamim@gmail.com>
2022-06-21 12:01:01 +0600

    border-radius: updated

--- components/WhyUs/WhyUs.jsx
+++ components/WhyUs/WhyUs.jsx
@@ -11,26 +11,26 @@ const WhyUs = () => {
         <div>
             <div className={classes.whyContainer}>
                 <div>
-                    <Image src={services} height="100px" width="100px"></Image>
+                    <Image src={services} height={50} width={50}></Image>
                     <br />
                     <CountUp start={10} end={100} duration={5} useEasing={true} className={classes.Counter} />
                     <p>Services</p>
                 </div>
                 <div>
-                    <Image src={hospital} height="100px" width="100px"></Image>
+                    <Image src={hospital} height={50} width={50}></Image>
                     <br />
                     <CountUp start={0} end={30} duration={5} useEasing={true} className={classes.Counter} />
                     <p>+ Hospitals</p>
                 </div>
 
                 <div>
-                    <Image src={doctor} height="100px" width="100px"></Image>
+                    <Image src={doctor} height={50} width={50}></Image>
                     <br />
                     <CountUp start={10} end={50} duration={5} useEasing={true} className={classes.Counter} />
                     <p>Doctors</p>
                 </div>
                 <div>
-                    <Image src={patients} height="100px" width="100px"></Image>
+                    <Image src={patients} height={50} width={50}></Image>
                     <br />
                     <CountUp start={10} end={1000} duration={5} useEasing={true} className={classes.Counter} />
                     <p>Patients</p>
