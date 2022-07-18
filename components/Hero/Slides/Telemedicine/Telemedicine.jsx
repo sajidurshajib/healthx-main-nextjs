da4c18ca27a8fb2ad787531b10e72190be1fb18e
Jahid11186 <zahidhasansamim@gmail.com>
2022-07-18 18:05:54 +0600

    doctor search bar created

--- components/Hero/Slides/Telemedicine/Telemedicine.jsx
+++ components/Hero/Slides/Telemedicine/Telemedicine.jsx
@@ -1,3 +1,5 @@
+import { faLocation, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classes from './Telemedicine.module.css'
 
 const Telemedicine = () => {
@@ -7,6 +9,29 @@ const Telemedicine = () => {
                 <h2>Telemedicine Service</h2>
                 <p>Consult with Top Doctors Online, 24x7</p>
             </div>
+            <form className={classes.wrapperRight}>
+                <div className={classes.searchBar}>
+                    <span>
+                        <FontAwesomeIcon icon={faLocation} height={10} width={15} />
+                    </span>
+                    <input type="text" placeholder="location" />
+                </div>
+                <div className={classes.searchBar}>
+                    <span>
+                        <FontAwesomeIcon icon={faSearch} height={10} width={15} />
+                    </span>
+                    <input type="text" placeholder="speciality" />
+                </div>
+                <div className={classes.searchBar}>
+                    <span>
+                        <FontAwesomeIcon icon={faSearch} height={10} width={15} />
+                    </span>
+                    <input type="text" placeholder="Name" />
+                </div>
+                <div className={classes.Search}>
+                    <button>Search</button>
+                </div>
+            </form>
         </div>
     )
 }
