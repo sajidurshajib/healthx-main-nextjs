3adb807c227acd65a0132cd20374f4a0dcae31df
morshedul-antor <antor.morshedul@gmail.com>
2022-07-24 18:14:13 +0600

    feat: single plan page component add

---
+++ components/SinglePlan/SinglePlan.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Detail from './Detail/Detail'
+
+export default function SinglePlan() {
+    return (
+        <div>
+            <Detail />
+        </div>
+    )
+}
---
+++ pages/plan.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MainNav, SinglePlan } from '../components'
+
+export default function plan() {
+    return (
+        <div>
+            <MainNav />
+            <SinglePlan />
+        </div>
+    )
+}
