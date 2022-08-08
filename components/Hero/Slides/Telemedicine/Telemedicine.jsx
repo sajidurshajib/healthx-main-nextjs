bb5529e5b4ac8bfcbc835e67cf65c90b7bb743fe
morshedul-antor <antor.morshedul@gmail.com>
2022-08-08 17:10:24 +0600

    feat: hero section image add with design

--- components/Hero/Slides/Telemedicine/Telemedicine.jsx
+++ components/Hero/Slides/Telemedicine/Telemedicine.jsx
@@ -1,6 +1,8 @@
 import { faLocation, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
 import { useState, useEffect } from 'react'
+import Pic from '../../../../assets/bg/telemedicine.png'
 import DoctorSearch from './DoctorSearch/DoctorSearch'
 import classes from './Telemedicine.module.css'
 
@@ -9,9 +11,14 @@ const Telemedicine = () => {
         <>
             <DoctorSearch />
             <div className={`${classes.Telemedicine} ${classes.container}`}>
-                <div className={classes.Wrapper}>
-                    <h2>Telemedicine Service</h2>
-                    <p>Consult with Top Doctors Online, 24x7</p>
+                <div className={classes.grid}>
+                    <div className={classes.Wrapper}>
+                        <h2>Telemedicine Service</h2>
+                        <p>Consult with Top Doctors Online, 24x7</p>
+                    </div>
+                    <div className={classes.imgWrapper}>
+                        <Image src={Pic} width={460} height={560} />
+                    </div>
                 </div>
             </div>
         </>
