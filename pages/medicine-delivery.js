193978eedbcb1aadf8e27583e2cda9a385ac98de
morshedul-antor <antor.morshedul@gmail.com>
2022-08-17 16:48:04 +0600

    feat: medicine order page add

---
+++ pages/medicine-delivery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MainNav, Medicine } from '../components'
+
+export default function medicine_delivery() {
+    return (
+        <div>
+            <MainNav />
+            <Medicine />
+        </div>
+    )
+}
