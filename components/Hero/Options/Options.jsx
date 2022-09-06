f5344da35631c54252bf12f19a6e27208800cbe2
morshedul-antor <antor.morshedul@gmail.com>
2022-09-06 17:40:33 +0600

    change: telemedicine descriptions

--- components/Hero/Options/Options.jsx
+++ components/Hero/Options/Options.jsx
@@ -19,7 +19,7 @@ const Options = ({ id, setId }) => {
                 <Select number={1} id={id} setId={setId}>
                     <FontAwesomeIcon icon={faPhone} height={18} width={18} />
                     <h4>Tele Health</h4>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, libero?</p>
+                    <p>HEALTHx connects you with GP & specialize doctors 24x7</p>
                 </Select>
                 <Select number={2} id={id} setId={setId}>
                     <FontAwesomeIcon icon={faCapsules} height={18} width={18} />
@@ -34,10 +34,10 @@ const Options = ({ id, setId }) => {
                 <Select number={4} id={id} setId={setId}>
                     <FontAwesomeIcon icon={faChartLine} height={18} width={18} />
                     <h4>Health Plan</h4>
-                    <p>Choose the best health package for your family</p>
+                    <p>Choose the best health plan for your family</p>
                 </Select>
                 <Select number={5} id={id} setId={setId}>
-                    <FontAwesomeIcon icon={faHandHolding} height={18} width={18} />
+                    <FontAwesomeIcon icon={faHeartPulse} height={18} width={18} />
                     <h4>Care Giving</h4>
                     <p>Get trusted & quality caregiving at home</p>
                 </Select>
