b752c2f0e36595fdeda395c15055a8b391ae73ae
morshedul-antor <antor.morshedul@gmail.com>
2022-10-27 17:37:57 +0600

    feat: hero part mobile responsive

--- components/Hero/Slides/Telemedicine/Telemedicine.jsx
+++ components/Hero/Slides/Telemedicine/Telemedicine.jsx
@@ -98,29 +98,8 @@ const Telemedicine = () => {
                                 <button>Book Now!</button>
                             </a>
                         </Link>
-                        {/* <div>
-                            <h5>
-                                <FontAwesomeIcon icon={faKitMedical} /> Family Medicine
-                            </h5>
-                            <h5>
-                                <FontAwesomeIcon icon={faBone} /> Orthopedics
-                            </h5>
-                            <h5>
-                                <FontAwesomeIcon icon={faHeartbeat} /> Cardiology
-                            </h5>
-                            <h5>
-                                <FontAwesomeIcon icon={faHandDots} /> Dermatology
-                            </h5>
-                        </div> */}
-                        {/* <div>
-                            <p>Popular Searches: </p>
-                            <span>medines, vd, skin, kidney</span>
-                        </div> */}
                     </div>
                     <div className={classes.search}>
-                        {/* <h4>
-                            <FontAwesomeIcon icon={faStethoscope} /> Search Doctors
-                        </h4> */}
                         <div>
                             <Image src={Tele} width={480} height={450} alt="" />
                         </div>
