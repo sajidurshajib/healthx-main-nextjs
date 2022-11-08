c8db8031092e451396fd8126f84ac121fab2a468
morshedul-antor <antor.morshedul@gmail.com>
2022-11-08 17:56:56 +0600

    feat: medicine line component add

---
+++ components/Medicine/MedicineLine/MedicineLine.jsx
@@ -0,0 +1,51 @@
+import '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+
+export default function MedicineLine({ med, medicineLines, setMedicineLines, index, removeItem }) {
+    const btnIncrease = () => {
+        setQty((prev) => prev + 1)
+    }
+    const btnDecrease = () => {
+        if (qty > 1) setQty((prev) => prev - 1)
+    }
+
+    const handleQuantity = (value) => {
+        let lines = medicineLines
+        lines[index].quantity = value
+        lines[index].price = med.unit_price
+        lines[index].total = lines[index].quantity * med.unit_price
+        setMedicineLines([...lines])
+    }
+
+    return (
+        <>
+            <span>{index + 1}</span>
+            <span>{med.name}</span>
+            <div>
+                {/* <button onClick={() => btnDecrease()}>-</button> */}
+                <input
+                    type="number"
+                    value={medicineLines[index].quantity}
+                    onChange={(e) => {
+                        handleQuantity(parseInt(e.target.value))
+                    }}
+                    required
+                    min={1}
+                />
+                {/* <button onClick={() => btnIncrease()}>+</button> */}
+            </div>
+            <span>{med.strength}</span>
+            <span>{med.form}</span>
+            <span>৳{med.unit_price}</span>
+            <span>
+                ৳
+                {isNaN(medicineLines[index].total + 0) !== true
+                    ? medicineLines[index].total.toFixed(2)
+                    : med.unit_price}
+            </span>
+            <FontAwesomeIcon icon={faTrashAlt} onClick={() => removeItem(index)} />
+        </>
+    )
+}
