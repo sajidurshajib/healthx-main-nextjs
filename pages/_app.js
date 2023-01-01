9111782c688acb7c9aa7b4ecc4fcc00b09322dc9
morshedul-antor <antor.morshedul@gmail.com>
2023-01-01 16:48:34 +0600

    add: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { useReducer, useEffect } from 'react'
 import TagManager from 'react-gtm-module'
 import { Auth, UserInfo } from '../context/allContext'
@@ -39,11 +40,30 @@ function MyApp({ Component, pageProps }) {
     }, [])
 
     return (
-        <Auth.Provider value={{ stateAuth, dispatchAuth }}>
-            <UserInfo.Provider value={{ stateUser, dispatchUser }}>
-                <Component {...pageProps} />
-            </UserInfo.Provider>
-        </Auth.Provider>
+        <>
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=UA-252658937-1"></Script>
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                     window.dataLayer = window.dataLayer || [];
+                     function gtag(){dataLayer.push(arguments);}
+                     gtag('js', new Date());
+                     gtag('config', 'UA-252658937-1', {
+                     page_path: window.location.pathname,
+                    });
+                    `,
+                }}
+            />
+            <Auth.Provider value={{ stateAuth, dispatchAuth }}>
+                <UserInfo.Provider value={{ stateUser, dispatchUser }}>
+                    <Component {...pageProps} />
+                </UserInfo.Provider>
+            </Auth.Provider>
+        </>
     )
 }
 
